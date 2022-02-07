6c85c9cfda3015abcbf0d7327d920afd09232506
toyoboh <st.0220@softbank.ne.jp>
2022-02-07 15:21:22 +0900

    リファクタリング（処理結果は変更なし）

--- master/board/food/bin/main.py
+++ master/board/food/bin/main.py
@@ -3,31 +3,37 @@ sys.path.append('../../../../')
 from common.common import patterns_replace
 import urllib.request
 from bs4 import BeautifulSoup
+import requests
 
 # 抽出対象URL
 url = 'https://www.pusannavi.com/special/5047301/index.html'
 
-with urllib.request.urlopen(url) as req_file:
-    # html解析
-    html_file = req_file.read()
-    soup      = BeautifulSoup(html_file, 'html.parser')
+# 料理名を格納する配列
+food_list = []
 
-    # 料理名が入っているelementの取得
-    food_elements     = soup.find_all(class_='atc_r_ttl')
-    food_elements_num = len(food_elements)
+# 対象URLにアクセスしデータを取得およびhtml解析
+res_data = requests.get(url)
+res_data.encoding = res_data.apparent_encoding # 文字化け対策
+soup = BeautifulSoup(res_data.text, 'html.parser')
 
-    # 料理名を取り出し、txtファイルに書き込む
-    with open('../data/information.txt', 'w') as list_file:
-        for i in range(food_elements_num):
-            # 料理名の抽出
-            food_name = food_elements[i].text
+# 料理名が入っている全部の要素（タグ）取得
+food_elements = soup.find_all(class_='atc_r_ttl')
 
-            # 不要な文字列を取り除く
-            clean_food_name = patterns_replace(food_name, [' ', '　'], "")
-            
-            # 最後の要素以外は改行文字をつける
-            if i != food_elements_num -1:
-                clean_food_name = clean_food_name + '\n'
-            
-            # テキストファイルに書き込む
-            list_file.write(clean_food_name)
+# 取得された要素から料理名を取得（不要な文字は取り除く）し、料理名配列に格納する
+for element in food_elements:
+    food_name = element.text
+    clean_food_name = patterns_replace(food_name, [' ', '　', '\n'], '')
+
+    food_list.append(clean_food_name)
+
+# ファイルに料理名を書き込む
+with open('../data/information.txt', 'w') as information_file:
+    food_list_num       = len(food_list)    # 配列要素数を取得
+    list_last_index_num = food_list_num - 1 # 配列の最後のインデックス番号を取得
+
+    for i in range(food_list_num):
+        # 配列の最後の料理名以外は、改行文字を後ろにつける
+        if i == list_last_index_num:
+            information_file.write(food_list[i])
+        else:
+            information_file.write(food_list[i] + '\n')
