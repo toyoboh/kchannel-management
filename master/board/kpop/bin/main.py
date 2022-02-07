01fc9eca9693eab3a203d1988104665db12fd126
toyoboh <st.0220@softbank.ne.jp>
2022-02-07 14:49:56 +0900

    ファイル記載の際に最終行は改行文字を含めないよう修正

--- master/board/kpop/bin/main.py
+++ master/board/kpop/bin/main.py
@@ -7,29 +7,43 @@ res_data = requests.get(url)
 # html解析
 soup = BeautifulSoup(res_data.text, 'html.parser')
 
+# kpopのグループ名を格納する配列
+group_list = []
+
+# 年代別にグループがまとめられているので、全テーブルからグループ名を取得し、配列に格納する
+for table in soup.find_all(class_='wikitable'):
+    # テーブルの全行と行数を取得
+    group_detail = table.find_all('tr')
+    group_detail_num = len(group_detail)
+    
+    # テーブルの先頭2行は不要なのでのぞいて処理をする
+    for i in range(2, group_detail_num):
+        # 行のtdと行に存在するtdの数を取得する
+        items = group_detail[i].find_all('td')
+        items_num = len(items)
+
+        # tdにてrowspanが指定されている行の場合、2番目のtdにグループ名がある
+        if(items_num == 8):
+            group_name = items[1].text
+        else:
+            group_name = items[0].text
+
+        # 改行文字の数がものによって異なるため、一度改行文字を削除する
+        clean_group_name = group_name.replace('\n', '')
+
+        # グループ名を配列に格納
+        group_list.append(clean_group_name)
+
 # kpopグループの名前を抽出し、ファイルに書き込む
+# for構文が1つ増えるため速度は多少遅くなるが、コードの見やすさを優先するためこの書き方とする
 with open("../data/information.txt", "w") as information_file:
+    # グループの要素数と配列の最後のインデックス番号を取得
+    group_list_num = len(group_list)
+    last_index_num = group_list_num - 1 # 配列が最後か判定するために使用
 
-    # 年代別にグループがまとめられているので、全テーブルを取得して処理をする
-    for table in soup.find_all(class_='wikitable'):
-        # テーブルの全行と行数を取得
-        group_detail = table.find_all('tr')
-        group_detail_num = len(group_detail)
-        
-        # テーブルの先頭2行は不要なのでのぞいて処理をする
-        for i in range(2, group_detail_num):
-            # 行のtdと行に存在するtdの数を取得する
-            items = group_detail[i].find_all('td')
-            items_count = len(items)
-
-            # tdにてrowspanが指定されている行の場合、2番目のtdにグループ名がある
-            if(items_count == 8):
-                group_name = items[1].text
-            else:
-                group_name = items[0].text
-
-            # 改行文字の数がものによって異なるため、一度改行文字を削除する
-            clean_group_name = group_name.replace('\n', '')
-
-            # ファイルに書き込む（改行文字を追加して）
-            information_file.write(clean_group_name + '\n')
+    for j in range(group_list_num):
+        # 配列の最後のみ改行文字を付け加えない
+        if j == last_index_num:
+            information_file.write(group_list[j])
+        else:
+            information_file.write(group_list[j] + '\n')
